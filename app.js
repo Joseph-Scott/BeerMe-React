14e33dc24ea8797dc0d38a8a77c63efc4f13eb38
Alex Anderson <alex@abanderson.com>
2018-07-27 18:35:34 -0400

    Minor changes to support API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var app = express();
 
+var beeradvocate = require('./routes/beeradvocate.js');
+var api = require('./routes/api.js');
+
 // view engine setup
 
 app.use(logger('dev'));
@@ -14,7 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.use('/beeradvocate', beeradvocate);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
