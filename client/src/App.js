83030c6f5c12746aef17302d23a5b6f2fa108534
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-07-28 11:59:15 -0400

    finishing up the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BeerCards from './BeerCards.js'
 import beerMeLogo from './img/beerpint.png';
 import './App.css';
 
@@ -6,20 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */} 
         
         <nav className="navbar">
             <div className="header-main">
 
                 <div className="logo">
-                    <a className="" href=""><img className="AppNavImg" src={beerMeLogo} alt="BeerMe! logo"></img>
-                    <span className="p-3 NavBeerMeLogo">BeerMe!</span></a>                    
+                    <a className="" href=""><img className="AppNavImg" src={beerMeLogo} alt="BeerMe! logo"></img></a>
+                    <span className="p-3 NavBeerMeLogo">BeerMe!</span>                    
                 </div>
 
                 <div className="navigation">
@@ -32,19 +26,11 @@ class App extends Component {
 
             </div>                
         </nav>
-        {/* <div className="container-fluid full-height">
-            <div className="row h-100">
-                <div className="col-lg-9 p-0">
-                    <div id="map"></div>
-                </div>
-                <div className="col-lg-3 mt-3">
-                    <form>
-                        <input id="myInput" onkeyup="search()" className="form-control" type="search" name="beermesearch" placeholder="Find a brew..."></input>
-                    </form>
-                    <div id="mySidebar" className="search-results"></div>
-                </div>
-            </div>
-        </div> */}
+        
+        <BeerCards>
+        </BeerCards>
+
+        
 
       </div>
     );
---
+++ client/src/BeerCards.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import App from './App.js'
+import './App.css';
+
+class BeerCards extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>    
+        )    
+    }
+}
+
+export default BeerCards;
