50a37f2772f53a516e0774f89160cf185c37675a
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-08-02 02:08:39 -0400

    styled login page for responsiveness and better design

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,32 +4,26 @@ class Login extends Component {
     render() {
         return (
             <div className="Login">
-                    <h1>Log in</h1>
+                    
                     <div className="login-form">
+                        <span>Login</span>
                         <form>
                             <div className="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="  Enter email"></input>
                             </div>
                             <div className="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="  Password"></input>
                             </div>
                             <div className="form-check">
                                 <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-                                <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                                <label className="form-check-label">Remember me</label>
                             </div>
-                            <a href="app.html"><button type="button" className="btn btn-primary btn-submit">Submit</button></a>
+                            <div className="form-button">
+                                <a href="/Results"><button type="button" className="btn"><span role="img" aria-label="beers clinking">🍻 </span>Submit <span role="img" aria-label="beers clinking">🍻</span></button></a>
+                            </div>
+                            
                         </form>
                     </div>
-                <div className="other-logins">
-                    <div className="facebook">
-                        <a href="app.html"><button type="button" className="btn btn-lg">Login with Facebook</button></a>
-                    </div>
-                    <div className="google">
-                        <a href="app.html"><button type="button" className="btn btn-lg">Login with Google</button></a>
-                    </div>            
-                </div>
             </div>    
         )    
     }
