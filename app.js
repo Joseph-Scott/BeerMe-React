dcf3edda8489f02b6d1eaa6b2514c2393531b11f
Alex Anderson <alex@abanderson.com>
2018-08-03 17:51:43 -0400

    Removed unnecessary code

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var app = express();
 
 var api = require("./routes/api.js");
 
-// view engine setup
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,18 +20,18 @@ app.use("/api", api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
